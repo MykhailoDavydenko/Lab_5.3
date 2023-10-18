4df8b8308d277d97c8bba4b400eafabf8a482d40
Mykhailo <mykhailo.davydenko.ik.2023@lpnu.ua>
2023-10-18 23:25:04 +0300

    dfghjkl

---
+++ Lab_5.3/Lab_5.3.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+double k(const double x);
+
+int main()
+{
+	double zp, zk,rez;
+	int n;
+	cout << "zp = "; cin >> zp;
+	cout << "zk = "; cin >> zk;
+	cout << "n = "; cin >> n;
+	double z_step = (zk - zp) / n;
+	double z = zp;
+
+	cout << "----------------------" << endl;
+	cout << "|" << setw(7) << "z" << " |"
+		<< setw(10) << "REZ" << " |" << endl;
+	cout << "----------------------" << endl;
+
+
+	while (z <= zk)
+	{
+		rez = k(z*z+1)-k(z*z-1)+2*k(z);
+		
+		cout <<"|" << setw(7) << setprecision(2) << z << " |"
+			<< setw(10) << setprecision(5) << rez <<" |" << endl;
+		z += z_step;
+	}
+	cout << "----------------------" << endl;
+	cout << k(2) << endl;
+	cout << k(0.5) << endl;
+	return 0;
+}
+double k(const double x)
+{
+	if (abs(x) >= 1)
+		return (exp(x) + sin(x)) / (cos(x)* cos(x) + 1);
+	else
+	{
+		double S = 0;
+		int i = 0;
+		double a = 1;
+		S = a;
+		do
+		{
+			i++;
+			double R = x/i;			
+			a *= R;
+			S += a;
+		} while (i < 5);
+		return (1./ exp(x))*S;
+	}
+}
+
+
+
+
+
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_5.3/Lab_5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+    TEST_CLASS(KFunctionTest)
+    {
+    public:
+
+        TEST_METHOD(TestKFunction)
+        {
+            // Перевірка для x >= 1
+            double x1 = 2.0;                       
+            Assert::AreEqual(k(x1), 7.0734, 0.00001);
+
+            // Перевірка для x < 1
+            double x2 = 0.5;          
+            Assert::AreEqual(k(x2), 0.99999, 0.00001);
+        }
+
+     
+    };
+}
